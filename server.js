82ffc9ab645ce4b9cb6d4d55f160f2b22388cf04
priya2201 <priyamurpani99@gmail.com>
2023-01-27 10:57:33 +0530

    practice

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 const express = require("express");
+const cors=require('cors');
 const app = express();
+var corsOptions={
+origin:"http://localhost:8081"
+}
+app.use(cors(corsOptions));
+// parse requests of content-type - application/json
 app.use(express.json());
+// parse requests of content-type - application/x-www-form-urlencoded
 app.use(express.urlencoded({ extended: true }));
 const db=require('./models');
 db.mongoose
@@ -16,7 +23,7 @@ db.mongoose
     process.exit();
   });
 
-
+require('./routes/tutorialroutes')(app);
 // simple route
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to bezkoder application." });
